7f18d323c0e6e2ce48a78f6de28e6df1e14cdfdf
triggereddown <deepmoitra2@gmail.com>
2024-11-23 22:27:01 +0530

    Learnt React routing for dashboard and landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, redirect, Route, Routes, useHref, useNavigate } from 'react-router-dom'
 import { Landing } from './components/Landing'
 import { Dashboard } from './components/Dashboard'
 
@@ -11,10 +11,18 @@ function App() {
 
   return (
     <div>
-      <div style={{background: "grey", color:"White"}}>
-        Constant top bar
-      </div>
+
+      {/* OLD WAY OF WINDOWS UPDATE */}
+      {/* <div >
+        <button onClick={()=>{
+          window.location.href="/";
+        }}>Landing Page</button>
+         <button onClick={()=>{
+    window.location.href="/dashboard";     
+        }}> Dshboard</button>
+      </div> */}
     <BrowserRouter>
+    <Appbar/>
     <Routes>
       <Route path = "/dashboard" element = {<Dashboard/>}/>
       <Route path = "/" element = {<Landing/>}/>
@@ -24,5 +32,32 @@ function App() {
     </div>
   )
 }
+
+
+function Appbar(){
+  const navigate=useNavigate();
+
+  return (
+    <div>
+      {/* This method removes not required reload on eevery button click */}
+      <div>
+        <button onClick={()=>{ 
+          navigate("/");
+        }}>
+          Landiing Page
+        </button>
+
+        <button onClick={ () =>{
+          navigate("/dashboard");
+        }}>
+          Dashboard Page
+        </button>
+      </div>
+    </div>
+  )
+}
+
+
+
 
 export default App
